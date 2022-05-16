c584099db0eef83d4fbd4d7f41cb134d136537c9
ATHOLOKAM <91723584+ATHOLOKAM@users.noreply.github.com>
2022-05-16 14:51:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Aadhi000/File-To-L.git /File-To-Link
+  git clone https://github.com/Aadhi000/File-To-Linkkk.git /
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /File-To-Link
+  git clone $UPSTREAM_REPO /File-To-Linkkk
 fi
-cd /EvaMaria
+cd /File-To-Linkkk
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "Starting....🔥"
 python3 bot.py
